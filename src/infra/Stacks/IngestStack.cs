9be4919f59af819e01c91544b24186997373b362
Greg Boyles <geboyles@gmail.com>
2026-03-25 20:54:29 +0000

    Remove FFmpeg layer, fix circular dependency in CDK stacks

--- src/infra/Stacks/IngestStack.cs
+++ src/infra/Stacks/IngestStack.cs
@@ -2,7 +2,6 @@ using Amazon.CDK;
 using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.S3;
-using Amazon.CDK.AWS.S3.Notifications;
 using Constructs;
 
 namespace SnoutSpotter.Infra.Stacks;
@@ -17,19 +16,15 @@ public class IngestStack : Stack
 {
     public IngestStack(Construct scope, string id, IngestStackProps props) : base(scope, id, props)
     {
-        // FFmpeg Lambda layer (community-maintained)
-        var ffmpegLayer = LayerVersion.FromLayerVersionArn(this, "FfmpegLayer",
-            $"arn:aws:lambda:{Region}:017000801446:layer:AWSLambda-Python-FFmpeg:1");
-
+        // Note: FFmpeg layer removed - keyframe extraction can be added later with custom layer
         var ingestFunction = new Function(this, "IngestClipFunction", new FunctionProps
         {
             FunctionName = "snout-spotter-ingest-clip",
             Runtime = Runtime.DOTNET_8,
             Handler = "SnoutSpotter.Lambda.IngestClip::SnoutSpotter.Lambda.IngestClip.Function::FunctionHandler",
             Code = Code.FromAsset("../lambdas/SnoutSpotter.Lambda.IngestClip/bin/Release/net8.0/publish"),
-            MemorySize = 1024,
-            Timeout = Duration.Minutes(5),
-            Layers = new[] { ffmpegLayer },
+            MemorySize = 512,
+            Timeout = Duration.Minutes(2),
             Environment = new Dictionary<string, string>
             {
                 ["BUCKET_NAME"] = props.DataBucket.BucketName,
@@ -41,14 +36,11 @@ public class IngestStack : Stack
         props.DataBucket.GrantReadWrite(ingestFunction);
         props.ClipsTable.GrantWriteData(ingestFunction);
 
-        // S3 trigger: invoke Lambda on new .mp4 files in raw-clips/
-        props.DataBucket.AddEventNotification(
-            EventType.OBJECT_CREATED,
-            new LambdaDestination(ingestFunction),
-            new NotificationKeyFilter
-            {
-                Prefix = "raw-clips/",
-                Suffix = ".mp4"
-            });
+        // Output the function ARN so we can manually configure S3 event notification
+        _ = new CfnOutput(this, "IngestFunctionArn", new CfnOutputProps
+        {
+            Value = ingestFunction.FunctionArn,
+            Description = "ARN of the IngestClip Lambda function (configure S3 event manually)"
+        });
     }
 }
