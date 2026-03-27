cdf7007f2a657d8791f75a1a81e4406fe300fd1f
Greg Boyles <geboyles@gmail.com>
2026-03-27 23:27:35 +0000

    Add PiMgmtStack and wire up in CDK - Create PiMgmtStack with Lambda, API Gateway, IAM permissions - Wire up PiMgmtStack in Program.cs with IoTStack and CoreStack dependencies - Lambda has all necessary IoT control plane permissions - Separate API Gateway endpoint for device provisioning operations - Environment variables: IOT_THING_GROUP, IOT_POLICY_NAME passed from IoTStack

---
+++ src/infra/Stacks/PiMgmtStack.cs
@@ -0,0 +1,120 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.Apigatewayv2;
+using Amazon.CDK.AWS.ECR;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
+using Constructs;
+
+namespace SnoutSpotter.Infra.Stacks;
+
+public class PiMgmtStackProps : StackProps
+{
+    public required Repository PiMgmtEcrRepo { get; init; }
+    public required string ImageTag { get; init; }
+    public required string IoTThingGroupName { get; init; }
+    public required string IoTPolicyName { get; init; }
+}
+
+public class PiMgmtStack : Stack
+{
+    public PiMgmtStack(Construct scope, string id, PiMgmtStackProps props) : base(scope, id, props)
+    {
+        var ecrRepo = props.PiMgmtEcrRepo;
+
+        // Lambda function for Pi Management API
+        var piMgmtFunction = new DockerImageFunction(this, "PiMgmtFunction", new DockerImageFunctionProps
+        {
+            FunctionName = "snout-spotter-pi-mgmt",
+            Description = "SnoutSpotter Pi Management API for device registration",
+            Code = DockerImageCode.FromEcr(ecrRepo, new EcrImageCodeProps
+            {
+                TagOrDigest = props.ImageTag
+            }),
+            MemorySize = 512,
+            Timeout = Duration.Seconds(30),
+            Environment = new Dictionary<string, string>
+            {
+                ["IOT_THING_GROUP"] = props.IoTThingGroupName,
+                ["IOT_POLICY_NAME"] = props.IoTPolicyName,
+                ["AWS_LWA_PORT"] = "8080"
+            }
+        });
+
+        // IAM permissions for IoT control plane operations
+        piMgmtFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps
+        {
+            Effect = Effect.ALLOW,
+            Actions = new[]
+            {
+                "iot:CreateThing",
+                "iot:DeleteThing",
+                "iot:CreateKeysAndCertificate",
+                "iot:DeleteCertificate",
+                "iot:UpdateCertificate",
+                "iot:AttachPolicy",
+                "iot:DetachPolicy",
+                "iot:AttachThingPrincipal",
+                "iot:DetachThingPrincipal",
+                "iot:AddThingToThingGroup",
+                "iot:RemoveThingFromThingGroup",
+                "iot:ListThingPrincipals",
+                "iot:ListThingsInThingGroup",
+                "iot:DescribeEndpoint"
+            },
+            Resources = new[] { "*" } // IoT control plane doesn't support resource-level permissions
+        }));
+
+        // HTTP API Gateway for Pi Management
+        var httpApi = new CfnApi(this, "PiMgmtApi", new CfnApiProps
+        {
+            Name = "snout-spotter-pi-mgmt",
+            ProtocolType = "HTTP",
+            CorsConfiguration = new CfnApi.CorsProperty
+            {
+                AllowOrigins = new[] { "*" },
+                AllowMethods = new[] { "GET", "POST", "DELETE", "OPTIONS" },
+                AllowHeaders = new[] { "*" },
+                MaxAge = 3600
+            }
+        });
+
+        // Lambda integration
+        var integration = new CfnIntegration(this, "PiMgmtIntegration", new CfnIntegrationProps
+        {
+            ApiId = httpApi.Ref,
+            IntegrationType = "AWS_PROXY",
+            IntegrationUri = piMgmtFunction.FunctionArn,
+            PayloadFormatVersion = "2.0"
+        });
+
+        // Default route: proxy all requests to Lambda
+        _ = new CfnRoute(this, "DefaultRoute", new CfnRouteProps
+        {
+            ApiId = httpApi.Ref,
+            RouteKey = "$default",
+            Target = $"integrations/{integration.Ref}"
+        });
+
+        // Auto-deploy stage
+        _ = new CfnStage(this, "PiMgmtStage", new CfnStageProps
+        {
+            ApiId = httpApi.Ref,
+            StageName = "$default",
+            AutoDeploy = true
+        });
+
+        // Grant API Gateway permission to invoke Lambda
+        piMgmtFunction.AddPermission("ApiGatewayInvoke", new Permission
+        {
+            Principal = new ServicePrincipal("apigateway.amazonaws.com"),
+            SourceArn = $"arn:aws:execute-api:{Region}:{Account}:{httpApi.Ref}/*"
+        });
+
+        // Outputs
+        _ = new CfnOutput(this, "PiMgmtApiUrl", new CfnOutputProps
+        {
+            Value = $"https://{httpApi.AttrApiEndpoint}",
+            Description = "Pi Management API Gateway endpoint URL"
+        });
+    }
+}
