bd623e5a44c13985ec5b2dbf572f44ccacb87819
Greg Boyles <geboyles@gmail.com>
2026-03-28 21:12:08 +0000

    Add Okta login flow to React frontend with token injection on API calls

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,13 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { Security } from "@okta/okta-react";
+import oktaAuth from "./auth/oktaConfig";
 import App from "./App";
 import "./index.css";
+
+function OktaWrapper() {
+  const navigate = useNavigate();
+  const restoreOriginalUri = async (
+    _oktaAuth: typeof oktaAuth,
+    originalUri: string,
+  ) => {
+    navigate(originalUri || "/", { replace: true });
+  };
+
+  return (
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <App />
+    </Security>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <OktaWrapper />
     </BrowserRouter>
   </StrictMode>,
 );
