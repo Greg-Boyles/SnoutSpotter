f8b6019a68e6a30658cb9a14e1bfd5f96cedb0f6
Greg Boyles <geboyles@gmail.com>
2026-03-28 23:21:59 +0000

    Add access policy and rule to Okta default authorization server
    Without a policy on the default auth server, Okta won't issue access tokens
    for the SPA app. Adds a policy scoped to the SnoutSpotter client ID with a
    rule allowing authorization_code grant for openid/profile/email scopes.

---
+++ terraform/okta/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "okta" {
+  org_name  = var.okta_org_name
+  base_url  = "okta.com"
+  api_token = var.okta_api_token
+}
+
+resource "okta_app_oauth" "snoutspotter" {
+  label                      = "SnoutSpotter"
+  type                       = "browser"
+  grant_types                = ["authorization_code"]
+  response_types             = ["code"]
+  token_endpoint_auth_method = "none"
+  pkce_required              = true
+
+  redirect_uris = [
+    "https://${var.cloudfront_domain}/login/callback",
+    "http://localhost:5173/login/callback",
+  ]
+
+  post_logout_redirect_uris = [
+    "https://${var.cloudfront_domain}",
+    "http://localhost:5173",
+  ]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Access policy on the default authorization server for the SPA app
+resource "okta_auth_server_policy" "snoutspotter" {
+  auth_server_id   = "default"
+  name             = "SnoutSpotter SPA"
+  description      = "Access policy for SnoutSpotter frontend"
+  priority         = 1
+  client_whitelist = [okta_app_oauth.snoutspotter.client_id]
+}
+
+resource "okta_auth_server_policy_rule" "snoutspotter" {
+  auth_server_id       = "default"
+  policy_id            = okta_auth_server_policy.snoutspotter.id
+  name                 = "Allow SPA access"
+  priority             = 1
+  grant_type_whitelist = ["authorization_code"]
+  scope_whitelist      = ["openid", "profile", "email"]
+  access_token_lifetime_minutes = 60
+}
