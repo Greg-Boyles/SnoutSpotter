a3202c68849bd91d4b9e2923a0e6cc37419754df
Greg Boyles <geboyles@gmail.com>
2026-04-04 00:30:57 +0100

    Tweak

---
+++ src/lambdas/SnoutSpotter.Lambda.PiMgmt/Services/DeviceRegistrationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace SnoutSpotter.Lambda.PiMgmt.Services;
+
+public class DeviceRegistrationResult
+{
+    public required string ThingName { get; init; }
+    public required string CertificatePem { get; init; }
+    public required string PrivateKey { get; init; }
+    public required string CertificateArn { get; init; }
+    public required string IoTEndpoint { get; init; }
+    public required string CredentialProviderEndpoint { get; init; }
+    public required string RootCaUrl { get; init; }
+}
